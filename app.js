81455a0377b480025e18c8fa2397345f455f7488
danielsason112 <danielsason112@gmail.com>
2020-11-01 19:48:35 +0200

    Initial commit.

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var config = require("./config");
-var swaggerUi = require('swagger-ui-express');
-var swaggerDocument = require('./swagger.json');
-var KeyValuePair = require("./lib/KeyValuePair");
 
 var route = require("./lib/route");
 
@@ -14,8 +11,6 @@ var jsonParser = bodyParser.json();
 app.use("/keyValueStore", jsonParser);
 
 app.use("/keyValueStore", route);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT || config.PORT, () => {
     console.log(`app started on port ${process.env.PORT || config.PORT}`);
