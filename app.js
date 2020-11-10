f9314926cd76574633cb2094fd1bba7c41df6716
danielsason112 <danielsason112@gmail.com>
2020-11-10 18:34:18 +0200

    Added dates format support.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var config = require("./config");
+//var swaggerUi = require('swagger-ui-express');
+//var swaggerDocument = require('./swagger.json');
+
 var seed = require("./lib/seedStorageService");
 
 var route = require("./lib/route");
@@ -14,6 +17,8 @@ seed();
 app.use("/keyValueStore", jsonParser);
 
 app.use("/keyValueStore", route);
+
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT || config.PORT, () => {
     console.log(`app started on port ${process.env.PORT || config.PORT}`);
